8b759f8a41fa3f5142c480efa661c1fafe8f842c
PEDRO HENRIQUE RODRIGUES DA SILVA <154482905+PhCybhor@users.noreply.github.com>
2026-01-03 08:42:05 -0300

    Add files via upload

---
+++ configfirebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB_4aAZBxQbdAH5BsWnIk9-5-zSr0hwHc4",
+    authDomain: "cybhor-b650a.firebaseapp.com",
+    databaseURL: "https://cybhor-b650a-default-rtdb.firebaseio.com",
+    projectId: "cybhor-b650a",
+    storageBucket: "cybhor-b650a.firebasestorage.app",
+    messagingSenderId: "737331307662",
+    appId: "1:737331307662:web:ebe1ec62c2d7a819c615df",
+    measurementId: "G-NVWX7SRPS6"
+  };
